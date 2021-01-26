e6b7cf3fe00d28fd1d725868ef27ac76113662eb
Vlad <malosay21@gmail.com>
2021-01-26 08:16:54 +0300

    com

--- KPIEP_Laba16/Program.cs
+++ KPIEP_Laba16/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace KPIEP_Laba16
 {
@@ -10,6 +11,40 @@ namespace KPIEP_Laba16
     {
         static void Main(string[] args)
         {
+            ArrayList list1 = new ArrayList() { 1, 3, "string", 'c', 2.0f };
+            ArrayList list2 = new ArrayList() { 2, 3, "string", 'a', 2.1f };
+
+
+            ArrayList rezList = PeresechenieSpiskov(list1, list2);
+
+            foreach (var item in rezList)
+            {
+                Console.Write(item + " ");
+            }
+
+            List<char> lChar = new List<char>() {'A','+','B' };
+            
+            
+
+
+            Console.ReadLine();
+        }
+
+        private static ArrayList PeresechenieSpiskov(ArrayList list1, ArrayList list2)
+        {
+            ArrayList list = new ArrayList();
+            for (int i = 0; i < list1.Count; i++)
+            {
+                for (int j = 0; j < list2.Count; j++)
+                {
+                    if (list1[i].Equals(list2[j]))
+                    {
+                        list.Add(list1[i]);
+                        break;
+                    }
+                }
+            }
+            return list;
         }
     }
 }
